efda47a75f727b63501bc510d47a533ac5f5dde5
cajeff4511 <jeffersonchristian259@gmail.com>
2025-06-15 20:55:32 -0500

    hello

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Lavafist1210145',
-  database: 'app'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
